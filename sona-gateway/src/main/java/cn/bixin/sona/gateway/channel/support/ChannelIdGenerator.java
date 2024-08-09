71a989a4a06d3cdb33d07e44bd40849c508f9b7b
shanglei <shanglei03@corp.netease.com>
2024-08-09 15:03:19 +0800

    feat(gateway): add log

--- sona-gateway/src/main/java/cn/bixin/sona/gateway/channel/support/ChannelIdGenerator.java
+++ sona-gateway/src/main/java/cn/bixin/sona/gateway/channel/support/ChannelIdGenerator.java
@@ -20,16 +20,27 @@ public final class ChannelIdGenerator {
     }
 
     public static String generateChannelId(InetSocketAddress remoteAddr, long timestamp) {
+        // 初始化一个容量为96的StringBuilder对象
         StringBuilder sb = new StringBuilder(96);
+        // 添加本地IP地址
         sb.append(NetUtil.LOCAL_IP_ADDR);
+        // 添加分隔符
         sb.append(CH_ID_SEPARATOR);
+        // 获取并添加远程IP地址
         sb.append(remoteAddr.getAddress().getHostAddress());
+        // 添加分隔符
         sb.append(CH_ID_SEPARATOR);
+        // 获取并添加远程端口
         sb.append(remoteAddr.getPort());
+        // 添加分隔符
         sb.append(CH_ID_SEPARATOR);
+        // 添加时间戳
         sb.append(timestamp);
+        // 添加分隔符
         sb.append(CH_ID_SEPARATOR);
+        // 获取并添加当前序列号的十六进制字符串表示
         sb.append(Integer.toHexString(SEQ.getAndIncrement()));
+        // 返回拼接好的字符串
         return sb.toString();
     }
 
